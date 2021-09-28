3f2a742b32508d293662ca6527635958df50ba63
Dinkar <dinkar035@gmail.in>
2021-09-28 20:38:44 +0530

    OneMore

---
+++ Personal/Courses/python-postgres/intro-to-databases/project/constants.py
@@ -0,0 +1,6 @@
+CONSUMER_KEY = 'mGcWjS3tjBmEutZ10LIQzfVgn'
+CONSUMER_SECRET = 'tQao3udXcGUN0j6Dj4N1PSmcWx7c7DWncnLhuZKuHfoJywhtdB'
+
+REQUEST_TOKEN_URL = 'https://api.twitter.com/oauth/request_token'
+ACCESS_TOKEN_URL = 'https://api.twitter.com/oauth/access_token'
+AUTHORIZATION_URL = 'https://api.twitter.com/oauth/authorize'
